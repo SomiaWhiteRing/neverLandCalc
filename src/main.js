641ba05fc8f8b4ee75e6271ee1c01a79773b9e52
苍旻 <55022033+SomiaWhiteRing@users.noreply.github.com>
2023-11-17 11:45:54 +0800

    加入analytics

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue'
+import { inject } from '@vercel/analytics';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+
+inject();
 
 new Vue({
   render: h => h(App),
